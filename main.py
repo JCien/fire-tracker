0dd1f79ecb01dacb16332f45179f503e65094b73
JCien <jcienjr@gmail.com>
2025-01-15 22:10:59 -0800

    Added data using the requests library and put it in a table using PrettyTable library

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from dotenv import load_dotenv
 from prettytable import PrettyTable
 import os
+import requests
 
 load_dotenv()
 
-api_key = os.getenv("FIRMS_MAP_KEY")
+payload_inactive = {'inactive': 'false'}
+payload_active = {'inactive': 'true'}
+active_only = True
+
+if (active_only):
+    r = requests.get('https://incidents.fire.ca.gov/umbraco/api/IncidentApi/List', params=payload_inactive)
+else:
+    r = requests.get('https://incidents.fire.ca.gov/umbraco/api/IncidentApi/List', params=payload_active)
+
+data = r.json()
 
 table = PrettyTable()
-table.field_names = ["Fire Name", "Size", "Containment %", "Proximity"]
+table.field_names = ["Fire Name", "Size (in acres)", "Containment %", "Date Started"]
 table.align = "c"
+
+for fire in data:
+    table.add_row([fire['Name'], fire['AcresBurned'], fire['PercentContained'], fire['Started']])
 
 def main():
     print(table.get_string())
